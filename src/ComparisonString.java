1ba783e19cccdbdc9f7e998fa8e4aff3db69fee0
nodal_tree <89542810+NodalTree@users.noreply.github.com>
2023-05-27 00:01:24 -0500

    Create ComparisonString.java
    prob B

---
+++ src/ComparisonString.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class ComparisonString {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        while(T-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            char[] s = st.nextToken().toCharArray();
+             //doing all the sample cases, you'll realize that you can start placing some number x, then the longest increasing or decreasing block + 1 is ur answer
+             int curr_ans =  1;
+             int ans = 1;//make sure u set this to one not 0 only weird thing to mess up on
+             for(int i = 1; i<N; i++){
+                 if(s[i] == s[i - 1]) curr_ans++;
+                 else curr_ans = 1;
+                 ans = Math.max(ans, curr_ans);
+             }
+             ans++;
+             pw.println(ans);
+        }
+        pw.close();
+    }
+    static final int mod = 1_000_000_007;
+    static final Random random = new Random();
+    static void ruffleSort(int[] a) {
+        int n = a.length;// shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+
+    static long add(long a, long b) {
+        return (a + b) % mod;
+    }
+
+    static long sub(long a, long b) {
+        return ((a - b) % mod + mod) % mod;
+    }
+
+    static long mul(long a, long b) {
+        return (a * b) % mod;
+    }
+
+    static long div(long a, long b) {
+        return mul(a, exp(b, mod - 2));
+    }
+    static long exp(long base, long exp) {
+        if (exp == 0)
+            return 1;
+        long half = exp(base, exp / 2);
+        if (exp % 2 == 0)
+            return mul(half, half);
+        return mul(half, mul(half, base));
+    }
+
+}
